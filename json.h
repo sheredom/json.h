48ac92f920b91011346a9352d118d8ba7d804a5c
Neil Henning <neil@duskborn.com>
2015-08-08 15:51:18 +0100

    Comment json.h to include relevant information on entry points and structs.

--- json.h
+++ json.h
@@ -50,12 +50,16 @@ extern "C" {
 
 struct json_value_s;
 
+// Parse a JSON text file, returning a pointer to the root of the JSON
+// structure. json_parse performs 1 call to malloc for the entire encoding.
+// Returns 0 if an error occurred (malformed JSON input, or malloc failed)
 json_pure struct json_value_s* json_parse(
   const void* src, size_t src_size);
 
 #undef json_pure
 #undef json_weak
 
+// The various types JSON values can be. Used to identify what a value is
 enum json_type_e {
   json_type_string,
   json_type_number,
@@ -66,29 +70,48 @@ enum json_type_e {
   json_type_null
 };
 
+// A JSON string value
 struct json_string_s {
+  // utf-8 string
   void* string;
+  // the size (in bytes) of the string
   size_t string_size;
 };
 
+// a JSON number value
 struct json_number_s {
+  // ASCII string containing representation of the number
   char* number;
+  // the size (in bytes) of the number
   size_t number_size;
 };
 
+// a JSON object value
 struct json_object_s {
+  // the names of the elements in the object
   struct json_string_s* names;
+  // the values of the named elements in the object
   struct json_value_s* values;
+  // the length of names and values (number of elements in the object)
   size_t length;
 };
 
+// a JSON array value
 struct json_array_s {
+  // the values of the elements in the array
   struct json_value_s* values;
+  // the length of values (number of elements in the array)
   size_t length;
 };
 
+// a JSON value
 struct json_value_s {
+  // a pointer to either a json_string_s, json_number_s, json_object_s, or
+  // json_array_s. Should be cast to the appropriate struct type based on what
+  // the type of this value is
   void* payload;
+  // Must be one of json_type_e. If type is json_type_true, json_type_false, or
+  // json_type_null, payload will be NULL
   size_t type;
 };
 
