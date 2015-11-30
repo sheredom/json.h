5a48f7a13c4c7b8344a6d54c1f893a0b63590181
Neil Henning <neil@duskborn.com>
2015-11-30 16:40:14 +0000

    Add a link at the top of the source files so that people who want to distribute the source files outwith github can still find the canonical source of for the json.h lib.

--- json.c
+++ json.c
@@ -1,3 +1,6 @@
+// The latest version of this library is available on GitHub;
+//   https://github.com/sheredom/json.h
+
 // This is free and unencumbered software released into the public domain.
 //
 // Anyone is free to copy, modify, publish, use, compile, sell, or
